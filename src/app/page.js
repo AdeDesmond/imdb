d2480e21dc7b4b84aafce91703cd7545df2fd7a8
AdeDesmond <desmondade09@gmail.com>
2023-09-28 14:47:00 +0800

    search movie from database

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import Results from "@/components/Results";
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+  const data = await response.json();
+  const results = data.results;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
+    <main className="">
+      <Results results={results} />
     </main>
   );
 }
