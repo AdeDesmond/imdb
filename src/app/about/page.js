ec0c69ee370130f40d69a191f2e5008909f6c089
AdeDesmond <desmondade09@gmail.com>
2023-09-28 13:46:49 +0800

    about page

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium dark:text-amber-500">About</h1>
+      <p>This is our movie database description</p>
+      <p>We will be doing lots of fun stuff during this period</p>
+    </div>
+  );
+}
+
+export default page;
